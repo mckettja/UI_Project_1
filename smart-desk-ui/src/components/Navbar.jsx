4fde2ec6bffd31ac2546a682a8e474e705f6ff00
Josh McKettrick <mckettja@mail.uc.edu>
2023-10-02 14:21:36 -0400

    starting app structure

---
+++ smart-desk-ui/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className='nav'>
+            <h1>Hello World!</h1>
+            <h2>Hello Josh</h2>
+        </div>
+    )
+}
+
+export default Navbar;
